7bcc1f966b70b60bfc12f3116948ec40d16fabce
Dylan <luxuandthebox@gmail.com>
2022-07-12 21:45:31 -0600

    Creation of Base Structs

---
+++ users.go
@@ -0,0 +1,102 @@
+package htbgo
+
+type BearerConnectionStatus struct {
+	
+}
+
+type BearerActiveMachine struct {
+	
+}
+
+type BearerFollowers struct {
+	
+}
+
+type BearerProfile struct {
+
+}
+
+type BearerSettings struct {
+
+}
+
+type BearerSubscriptions struct {
+
+}
+
+type BearerSubscriptionsBalance struct {
+
+}
+
+type BearerSubscriptionsRecurly struct {
+
+}
+
+type BearerTracks struct {
+
+}
+
+type BearerMachineSubmissions struct {
+
+}
+
+type BearerChallengeSubmissions struct {
+
+}
+
+type BearerMachinesTODO struct {
+
+}
+
+type BearerUserRelationship struct {
+
+}
+
+type UserProgressMachines struct {
+
+}
+
+type UserProgressChallenges struct {
+
+}
+
+type UserProgressEndgame struct {
+
+}
+
+type UserProgressFortress struct {
+
+}
+
+type UserProgressProLab struct {
+
+}
+
+type UserAllActivities struct {
+
+}
+
+type UserAllBloods struct {
+
+}
+
+type UserAllSubmissions struct {
+
+}
+
+type UserAllAchievements struct {
+
+}
+
+type UserAllAttackPaths struct {
+
+}
+
+type UserProfile struct {
+
+}
+
+type UserAllBadges struct {
+
+}
+
