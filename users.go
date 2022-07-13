423419a34dda0117b3fe05c47776afd1b5fa707d
Dylan <luxuandthebox@gmail.com>
2022-07-12 21:46:40 -0600

    Add Base User Methods

--- users.go
+++ users.go
@@ -100,3 +100,127 @@ type UserAllBadges struct {
 
 }
 
+func (s *Session) ConnectionStatus() (status BearerConnectionStatus) {
+
+	return
+}
+
+func (s *Session) ActiveMachine() (active BearerActiveMachine) {
+
+	return
+}
+
+func (s *Session) Followers() (followers BearerFollowers) {
+
+	return
+}
+
+func (s *Session) Profile() (profile BearerProfile) {
+
+	return
+}
+
+func (s *Session) Settings() (settings BearerSettings) {
+
+	return
+}
+
+func (s *Session) Subscriptions() (subscriptions BearerSubscriptions) {
+
+	return
+}
+
+func (s *Session) SubscriptionsBalance() (balance BearerSubscriptionsBalance) {
+
+	return
+}
+
+func (s *Session) SubscriptionsRecurly() (recurly BearerSubscriptionsRecurly) {
+
+	return
+}
+
+func (s *Session) Tracks() (tracks BearerTracks) {
+
+	return
+}
+
+func (s *Session) MachineSubmissions() (submissions BearerMachineSubmissions) {
+
+	return
+}
+
+func (s *Session) ChallengeSubmissions() (submissions BearerChallengeSubmissions) {
+
+	return
+}
+
+func (s *Session) MachinesTODO() (todo BearerMachinesTODO) {
+
+	return
+}
+
+func (s *Session) UserRelationship() (relationship BearerUserRelationship) {
+
+	return
+}
+
+func (s *Session) UserMachines() (machines UserProgressMachines) {
+
+	return
+}
+
+func (s *Session) UserChallenges() (challenges UserProgressChallenges) {
+
+	return
+}
+
+func (s *Session) UserEndgames() (endgames UserProgressEndgames) {
+
+	return
+}
+
+func (s *Session) UserFortresses() (fortresses UserProgressFortresses) {
+
+	return
+}
+
+func (s *Session) UserProLabs() (proLabs UserProgressProLab) {
+
+	return
+}
+
+func (s *Session) UserActivity() (activities UserAllActivities) {
+
+	return
+}
+
+func (s *Session) UserBloods() (bloods UserAllBloods) {
+
+	return
+}
+
+func (s *Session) UserSubmissions() (submissions UserAllSubmissions) {
+
+	return
+}
+
+func (s *Session) UserAchievements() (acheivements UserAllAchievements) {
+
+	return
+}
+
+func (s *Session) UserPathOwns() (owns UserAllAttackPaths) {
+
+	return
+}
+
+func (s *Session) UserProfile() (profile UserProfile) {
+
+	return
+}
+
+func (s *Session) UserBadges() (badges UserAllBadges) {
+
+	return
+}
