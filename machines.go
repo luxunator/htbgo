31a6d1e89d3f63d20bace0f93a272ceeb78c6c52
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-07-13 10:52:28 +0800

    Update machines.go
    Added binders for the following:
    - Todo Machines endpoint
    - Scheduled Machines endpoint

--- machines.go
+++ machines.go
@@ -1,68 +1,5 @@
 package htbgo
 
-// type maker struct {
-// 	ID                  int    `json:"id"`
-// 	Name                string `json:"name"`
-// 	Avatar              string `json:"avatar"`
-// 	AuthUserSentRespect bool   `json:"isRespected"`
-// }
-
-// type user struct {
-// 	Name   string `json:"name"`
-// 	ID     int    `json:"id"`
-// 	Avatar string `json:"avatar"`
-// }
-
-// type firstBlood struct {
-// 	User               user   `json:"user"`
-// 	MachineCreatedAt   string `json:"created_at"`
-// 	FirstBloodDuration string `json:"blood_difference"`
-// }
-
-// type machineInfo struct {
-// 	ID                  int    `json:"id"`
-// 	Name                string `json:"name"`
-// 	OS                  string `json:"os"`
-// 	IsTodo              bool   `json:"isTodo"`
-// 	Active              int    `json:"active"`
-// 	Retired             int    `json:"retired"`
-// 	IP                  string `json:"ip"`
-// 	Points              int    `json:"points"`
-// 	StaticPoints        int    `json:"static_points"`
-// 	Release             string `json:"release"`
-// 	UserOwnCounts       int    `json:"user_owns_count"`
-// 	RootOwnCounts       int    `json:"root_owns_count"`
-// 	Free                bool   `json:"free"`
-// 	AuthUserIfUserOwns  bool   `json:"authUserInUserOwns"`
-// 	AuthUserIfRootOwns  bool   `json:"authUserInRootOwns"`
-// 	AuthUserHasReviewed bool   `json:"authUserHasReviewed"`
-// 	Stars               string `json:"stars"`
-// 	Difficulty          int    `json:"difficulty"`
-// 	Avatar              string `json:"avatar"`
-// 	FeedbackChart       struct {
-// 		CakeDifficulty      int `json:"counterCake"`
-// 		VeryEasyDifficulty  int `json:"counterVeryEasy"`
-// 		EasyDifficulty      int `json:"counterEasy"`
-// 		TooEasyDifficulty   int `json:"counterTooEasy"`
-// 		MediumDifficulty    int `json:"counterMedium"`
-// 		BitHardDifficulty   int `json:"counterBitHard"`
-// 		HardDifficulty      int `json:"counterHard"`
-// 		TooHardDifficulty   int `json:"counterTooHard"`
-// 		ExtraHardDifficulty int `json:"counterExHard"`
-// 		BrainFuckDifficulty int `json:"counterBrainFuck"`
-// 	} `json:"feedbackForChart"`
-// 	DifficultyText string `json:"difficultyText"`
-// 	IsCompleted    bool   `json:"isCompleted"`
-// 	LastResetTime  string `json:"last_reset_time"`
-// 	PlayInfo       struct {
-// 		IsSpawned         bool   `json:"isSpawned"`
-// 		IsSpawning        bool   `json:"isSpawning"`
-// 		IsActive          bool   `json:"isActive"`
-// 		ActivePlayerCount int    `json:"active_player_count"`
-// 		ExpiresAt         string `json:"expires_at"`
-// 	}
-// }
-
 // Get Machine Matrix
 // https://www.hackthebox.com/api/v4/machine/graph/matrix/{machineID}
 type MachineMatrixInformation struct {
@@ -392,6 +329,107 @@ type TopUsers struct {
 	} `json:"info"`
 }
 
+// Get the list of scheduled machines to release
+// https://www.hackthebox.com/api/v4/machine/unreleased
+type ScheduledMachines struct {
+	Data []struct {
+		ID             int    `json:"id"`
+		Name           string `json:"name"`
+		OS             string `json:"os"`
+		Avatar         string `json:"avatar"`
+		Release        string `json:"release"`
+		Difficulty     int    `json:"difficulty"`
+		DifficultyText string `json:"difficulty_text"`
+		FirstCreator   []struct {
+			ID     int    `json:"id"`
+			Name   string `json:"name"`
+			Avatar string `json:"avatar"`
+		} `json:"firstCreator"`
+
+		CoCreators []struct {
+			ID     int    `json:"id"`
+			Name   string `json:"name"`
+			Avatar string `json:"avatar"`
+		} `json:"coCreators"`
+
+		Retiring struct {
+			DifficultyText string `json:"difficulty_text"`
+			Avatar         string `json:"avatar"`
+			OS             string `json:"os"`
+			Name           string `json:"name"`
+			ID             int    `json:"id"`
+		} `json:"retiring"`
+	}
+}
+
+// Get the list of machines using the todo endpoint (STUPID API)
+// https://www.hackthebox.com/api/v4/machine/list/todo
+
+type TodoMachines struct {
+	Info []struct {
+		ID                  int    `json:"id"`
+		Name                string `json:"name"`
+		OS                  string `json:"os"`
+		Points              int    `json:"points"`
+		StaticPoints        int    `json:"static_points"`
+		Release             string `json:"release"`
+		UserOwnCounts       int    `json:"user_owns_count"`
+		RootOwnCounts       int    `json:"root_owns_count"`
+		AuthUserIfUserOwns  bool   `json:"authUserInUserOwns"`
+		AuthUserIfRootOwns  bool   `json:"authUserInRootOwns"`
+		IsTodo              bool   `json:"isTodo"`
+		AuthUserHasReviewed bool   `json:"authUserHasReviewed"`
+		Stars               string `json:"stars"`
+		Difficulty          int    `json:"difficulty"`
+		FeedbackChart       struct {
+			CakeDifficulty      int `json:"counterCake"`
+			VeryEasyDifficulty  int `json:"counterVeryEasy"`
+			EasyDifficulty      int `json:"counterEasy"`
+			TooEasyDifficulty   int `json:"counterTooEasy"`
+			MediumDifficulty    int `json:"counterMedium"`
+			BitHardDifficulty   int `json:"counterBitHard"`
+			HardDifficulty      int `json:"counterHard"`
+			TooHardDifficulty   int `json:"counterTooHard"`
+			ExtraHardDifficulty int `json:"counterExHard"`
+			BrainFuckDifficulty int `json:"counterBrainFuck"`
+		} `json:"feedbackForChart"`
+
+		Avatar         string `json:"avatar"`
+		DifficultyText string `json:"difficultyText"`
+		PlayInfo       struct {
+			IsSpawned         bool   `json:"isSpawned"`
+			IsSpawning        bool   `json:"isSpawning"`
+			IsActive          bool   `json:"isActive"`
+			ActivePlayerCount int    `json:"active_player_count"`
+			ExpiresAt         string `json:"expires_at"`
+		} `json:"playInfo"`
+
+		Free         bool `json:"free"`
+		MakerPrimary struct {
+			ID                  int    `json:"id"`
+			Name                string `json:"name"`
+			Avatar              string `json:"avatar"`
+			AuthUserSentRespect bool   `json:"isRespected"`
+		} `json:"maker"`
+
+		MakerSecondary struct {
+			ID                  int    `json:"id"`
+			Name                string `json:"name"`
+			Avatar              string `json:"avatar"`
+			AuthUserSentRespect bool   `json:"isRespected"`
+		} `json:"maker2"`
+
+		Recommended int    `json:"recommended"`
+		SPFlag      int    `json:"sp_flag"`
+		EasyMonth   int    `json:"easy_month"`
+		IP          string `json:"ip"`
+		Tags        []struct {
+			ID             int `json:"id"`
+			TagCategoryIDs int `json:"tag_category_id"`
+		} `json:"tags"`
+	} `json:"info"`
+}
+
 func (s *Session) MachineMatrix(machineID string) (matrix MachineMatrixInformation) {
 	var url string = "https://www.hackthebox.com/api/v4/machine/graph/matrix/" + machineID
 	parseJSON(s, url, &matrix)
@@ -422,5 +460,16 @@ func (s *Session) RetiredMachines() (retiredMachines RetiredMachines) {
 func (s *Session) MachineTopUsers(machineID string) (topUsers TopUsers) {
 	var url string = "https://www.hackthebox.com/api/v4/machine/owns/top/" + machineID
 	parseJSON(s, url, &topUsers)
+	return
+}
+
+func (s *Session) ScheduledMachines() (scheduledMachines ScheduledMachines) {
+	var url string = "https://www.hackthebox.com/api/v4/machine/unreleased"
+	parseJSON(s, url, &scheduledMachines)
+	return
+}
+func (s *Session) TodoMachines() (todoMachines TodoMachines) {
+	var url string = "https://www.hackthebox.com/api/v4/machine/list/todo"
+	parseJSON(s, url, &todoMachines)
 	return
 }
